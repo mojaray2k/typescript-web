24b66243a752ef0888ca35663e1062761088a64d
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-25 13:50:09 -0400

    refactored the Collection class to be a generic class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import {Collection} from './models/Collection';
+import {User, UserProps} from './models/User';
 
-const collection = new Collection('http://localhost:3019/users');
+const collection = new Collection<User, UserProps>(
+  'http://localhost:3019/users',
+  (json: UserProps) => User.buildUser(json)
+);
 
 collection.on('change', () => {
   console.log(collection);
