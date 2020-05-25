cc0b9b59d90a9bac0e3a485a42de6254210ee2c2
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-25 18:04:48 -0400

    created Userform onSetNameClick event

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -14,11 +14,19 @@ export class UserForm {
   eventsMap(): {[key: string]: () => void} {
     return {
       'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
     };
   }
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
+  };
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+    const name = input.value;
+
+    this.model.set({name});
   };
 
   template(): string {
@@ -28,7 +36,7 @@ export class UserForm {
         <div>User Name: ${this.model.get('name')}</div>
         <div>User Age: ${this.model.get('age')}</div>
         <input />
-        <button>Click Me</button>
+        <button class="set-name">Change Name</button>
         <button class="set-age">Set Random Age</button>
       </div>
     `;
