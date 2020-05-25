68ce7fb52f3b88a482d3972214fbc4489bb36d39
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-24 21:48:42 -0400

    setup json server and concurrently

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,14 @@ import {User} from './models/User';
 
 const user = new User({name: 'FirstName LastName', age: 20});
 
-user.set({name: 'new LastName'});
+user.on('change', () => {
+  console.log('Change #1');
+});
+user.on('change', () => {
+  console.log('Change #2');
+});
+user.on('click', () => {
+  console.log('Click #1');
+});
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.trigger('click');
