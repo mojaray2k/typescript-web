36dd442cc3096026156f2b1d1f58b1e1e84803fa
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-26 00:09:46 -0400

    Created User Collection Class

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-import {UserEdit} from './views/UserEdit';
-import {User} from './models/User';
+import {UserList} from './views/UserList';
+import {Collection} from './models/Collection';
+import {UserProps, User} from './models/User';
 
-const user = User.buildUser({name: 'Name', age: 0});
-const root = document.getElementById('root');
-if (root) {
-  const userEdit = new UserEdit(root, user);
+const users = new Collection(
+  'http://localhost:3019/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-  userEdit.render();
-} else {
-  throw new Error('Root element not found');
-}
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
