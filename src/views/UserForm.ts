371c824f5eb020c7bb3c6d7786dc04407e23b2e3
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-25 21:04:20 -0400

    Finished up the UserForm Class

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,12 +1,18 @@
 import {View} from './View';
+import {User, UserProps} from '../models/User';
 
-export class UserForm extends View {
+export class UserForm extends View<User, UserProps> {
   eventsMap(): {[key: string]: () => void} {
     return {
       'click:.set-age': this.onSetAgeClick,
       'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick,
     };
   }
+
+  onSaveClick = (): void => {
+    this.model.save();
+  };
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
@@ -24,12 +30,10 @@ export class UserForm extends View {
   template(): string {
     return /*javascript*/ `
       <div>
-        <h1>User form</h1>
-        <div>User Name: ${this.model.get('name')}</div>
-        <div>User Age: ${this.model.get('age')}</div>
-        <input />
+        <input placeholder="${this.model.get('name')}" />
         <button class="set-name">Change Name</button>
         <button class="set-age">Set Random Age</button>
+        <button class="save-model">Save User</button>
       </div>
     `;
   }
